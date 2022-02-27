925e80eb4476543289d92bdfecd71e40f5eb061e
Сергей Начаров <nacharovsa@it2g.ru>
2022-02-28 00:21:53 +0400

    start

---
+++ src/assets/js/app.js
@@ -0,0 +1,147 @@
+$(() => {
+  $(window).on('load', function () {
+    $('.preloader__wrp').fadeOut();
+  });
+});
+
+$(() => {
+  $('.js-tel').mask("+7 (999) 999-99-99");
+
+  $(window)
+    .scroll(function windowScroll() {
+      if ($(this).scrollTop() > 4) {
+        $('body').addClass('sticky');
+      }
+      else {
+        $('body').removeClass('sticky');
+      }
+    });
+
+  $('.tabs-content-item:not(:first-child)').hide();
+  $('.js-tab--main').on('click', function() {
+    if (!($(this).hasClass('active'))) {
+      $('.js-tab--main').removeClass('active');
+      $(this).addClass('active');
+      $('.js-tabs-content-main').hide();$(this).parents('.tabs').find('.js-tabs-content-main[data-main="'+$(this).attr('data-main')+'"]').fadeIn().addClass('active');
+    }
+  });
+  $('.js-tab--add').on('click', function() {
+    if (!($(this).hasClass('active'))) {
+      $('.js-tab--add').removeClass('active');
+      $(this).addClass('active');
+      $('.js-tabs-content-add').hide();
+      $(this).parents('.tabs').find('.js-tabs-content-add[data-add="' + $(this).attr('data-add') + '"]').fadeIn().addClass('active');
+    }
+  });
+  if ( $(window).width() > 1200 ) {
+    $('.categories-item:first-child').addClass('active');
+    const firstBg = $('.categories-item:first-child').attr('data-bg');
+    $('.categories').css('background-image', `url(${firstBg})`);
+
+    $('.categories-item').hover(function () {
+      if (!($(this).hasClass('active')) && !($(this).parents('.categories').hasClass('changing'))) {
+        $('.categories-item').removeClass('active');
+        $(this).addClass('active');
+
+        $('.categories').addClass('changing');
+
+        setTimeout(() => {
+          const thisBg = $(this).attr('data-bg');
+          $('.categories')
+              .addClass('changing')
+              .css('background-image', `url(${thisBg})`);
+        }, 600)
+        setTimeout(() => {
+          $('.categories').removeClass('changing');
+        }, 800)
+      }
+    });
+  };
+
+  $('.js-toggle-menu').on('click', function () {
+    $(this).toggleClass('active');
+    $('body').toggleClass('menu-open fixed');
+  });
+
+  $('.js-toggle-search').on('click', function () {
+    if (!($(this).hasClass('active'))) {
+      $(this).addClass('active');
+      $('.header-search').fadeIn();
+    } else {
+      $(this).removeClass('active');
+      $('.header-search').fadeOut();
+    }
+
+  });
+  $(document).on('click', function (e) {
+    const container = $('.header');
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      $('.burger').removeClass('active');
+      $('body').removeClass('menu-open fixed');
+    }
+  });
+  $('.js-to-top').on('click', function () {
+    $('html, body').animate({scrollTop:0},700);
+  });
+
+  $('.accordion-content').hide();
+  $('.js-toggle-accordion').on('click', function () {
+    $(this).parents('.accordion').toggleClass('active').find('.accordion-content').slideToggle();
+  });
+
+  $('.question-content').hide();
+  $('.js-toggle-question').on('click', function () {
+    $(this).parents('.question').toggleClass('active').find('.question-content').slideToggle();
+  });
+
+  $('[data-fancybox]').fancybox({
+    animationDuration: 600,
+    animationEffect: 'slide-in-in',
+    touch: false,
+  });
+});
+
+$(() => {
+  const swiper = new Swiper(".js-news-page-slider-bottom", {
+    spaceBetween: 20,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  const swiper2 = new Swiper(".js-news-page-slider-top", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".js-news-page-next",
+      prevEl: ".js-news-page-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+})
+$(() => {
+  const swiper3 = new Swiper(".js-license-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    centeredSlides: true,
+    initialSlide: 2,
+    navigation: {
+      nextEl: ".js-license-next",
+      prevEl: ".js-license-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
+});
---
+++ src/assets/js/map.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  var myMap;
+  let center;
+  let zoom;
+  if ( $(window).width() > 1200 ) {
+    center = [47.399147, 39.127788];
+    zoom = [7.5];
+  } else {
+    center = [47.372560, 40.769866];
+    zoom = [7];
+  }
+  ymaps.ready(init);
+  function init(){
+    myMap = new ymaps.Map("ymap", {
+      center,
+      zoom,
+      // scrollZoom: false,
+      controls: ["typeSelector"],
+    });
+    myMap.behaviors.disable('drag');
+    myMap.behaviors.disable('scrollZoom');
+    var placemarks = [
+      {
+        coords: [47.185511, 39.656425],
+        icon: 'assets/img/baloon.svg',
+        icon_size: [62, 86],
+        icon_offset: [-31, -43], // -50% ширины, -100% высоты от точки привязки (левый верхний угол)
+      },
+      {
+        coords: [47.767689, 39.940580],
+        icon: 'assets/img/baloon.svg',
+        icon_size: [62, 86],
+        icon_offset: [-31, -43],
+      },
+      {
+        coords: [47.528465, 42.134257],
+        icon: 'assets/img/baloon.svg',
+        icon_size: [62, 86],
+        icon_offset: [-31, -43],
+      }
+    ];
+
+    placemarks.forEach(function(item){
+      var obj = new ymaps.Placemark(
+          item.coords,
+          {},
+          {
+            iconLayout: 'default#image',
+            iconImageHref: item.icon,
+            iconImageSize: item.icon_size,
+            iconImageOffset: item.icon_offset,
+          }
+      );
+      myMap.geoObjects.add(obj);
+    });
+  };
+});
